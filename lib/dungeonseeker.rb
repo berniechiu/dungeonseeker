2da438c3ce1ad86b7837b325aee1764223e5f0c6
Bernie Chiu <bernie_chiu@yahoo.com.tw>
2013-03-05 13:38:37 +0800

    fix executable file

--- lib/dungeonseeker.rb
+++ lib/dungeonseeker.rb
@@ -1,29 +1,27 @@
-class Engine
+module DungeonSeeker
 
-	# Make a new room class
-	def initialize(startRoom)
-		@room = startRoom.new()
-	end
+	class Engine
 
-	# Start the game
-	def play()
-		next_challenge = @room.get_first_challenge()
+		# Make a new room class
+		def initialize()
+			@room = PandaRoom.new()
+		end
 
-		while true
-			if next_challenge.kind_of? Class
-				@room = next_challenge.new()
-				next_challenge = @room.get_first_challenge()
-			else
-				puts "\n--------"
-				challenge = @room.method(next_challenge)
-				next_challenge = challenge.call()
+		# Start the game
+		def play()
+			next_challenge = @room.get_first_challenge()
+
+			while true
+				if next_challenge.kind_of? Class
+					@room = next_challenge.new()
+					next_challenge = @room.get_first_challenge()
+				else
+					puts "\n--------"
+					challenge = @room.method(next_challenge)
+					next_challenge = challenge.call()
+				end
 			end
 		end
-	end
-
-	# A test for gem in executable bin file.
-	def self.enterRoom(room)
-		puts "#{room}"
 	end
 end
 
